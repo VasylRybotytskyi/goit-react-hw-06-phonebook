09062baaf63a49dbc75ec6397db358039b40243d
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-25 21:37:18 +0300

    refactoring code

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 import { FilterWrapper, FilterLabel, FilterInput } from './Filter.styled';
+import { setFilter } from 'redux/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFilter } from 'redux/selectors';
 
-export const Filter = ({ onChange, value }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
   return (
     <FilterWrapper>
       <FilterLabel htmlFor="find">Find contacts by name</FilterLabel>
       <FilterInput
         type="text"
-        onChange={onChange}
-        value={value}
+        onChange={event => dispatch(setFilter(event.target.value.trim()))} // викликає редюсер з екшеном setFilter
+        value={filter}
         id="find"
         name="find"
       />
